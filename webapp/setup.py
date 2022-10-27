a52948bbfc8d8516d2921dc5234bf53033b8314a
srujanreddy88 <janagamsrujan88@gmail.com>
2022-10-27 03:00:16 +0000

    commited

---
+++ webapp/setup.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+# DB-API spec for talking to relational databases in Python
+
+connection = sqlite3.connect("shopping_list.db")
+
+cursor = connection.cursor()
+
+try:
+    cursor.execute("drop table list")
+except:
+    pass
+
+cursor.execute("create table list (id integer primary key, description text)")
+
+cursor.execute("insert into list (description) values ('cars')")
+cursor.execute("insert into list (description) values ('bikes')")
+cursor.execute("insert into list (description) values ('trucks')")
+cursor.execute("insert into list (description) values ('buses')")
+
+
+connection.commit()
+connection.close()
+
+print("done.")
